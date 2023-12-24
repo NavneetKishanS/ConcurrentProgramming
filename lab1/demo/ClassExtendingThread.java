eebcdd76291635dad79c86b4e851aa1a9fae16de
Navneet Kishan Srinivasan <115086283+NavneetKishanS@users.noreply.github.com>
2023-12-24 09:19:54 +0100

    initial commit

---
+++ lab1/demo/ClassExtendingThread.java
@@ -0,0 +1,15 @@
+/**
+ * Thread creation example 1 - Extending the Thread class
+ *
+ * Override the run() method to implement custom logic.
+ *
+ */
+public class ClassExtendingThread extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() +
+                ": The following are outputs of a class extending the Thread class");
+        DemoBasicThreadingHelper.printHellox100();
+    }
+}
---
+++ lab1/demo/ClassImplementingRunnable.java
@@ -0,0 +1,15 @@
+/**
+ * Thread creation example 2.1 - Implementing Runnable interface
+ *
+ * Override the run() method to implement custom logic.
+ *
+ */
+public class ClassImplementingRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() +
+                ": The following are outputs of the Thread class using a custom class that implements Runnable");
+        DemoBasicThreadingHelper.printHellox100();
+    }
+}
